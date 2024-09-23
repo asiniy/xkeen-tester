f19e465aa44f6a60b9114db4b7d71699a72ae9df
Alex Antonov <kaburbundokel11g@inbox.ru>
2024-09-23 09:34:50 +0300

    very initial

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
 import { Telegraf } from 'telegraf'
+import { z } from 'zod'
 
-const INTERVAL = (process.env.INTERVAL ? parseInt(process.env.INTERVAL) : 10) * 1000 // Polling interval (in seconds)
-const BOT_TOKEN = process.env.BOT_TOKEN
+const Env = z.object({
+  BOT_TOKEN: z.string().optional(),
+  IP: z.string().ip(),
+})
 
-let bot
-if (BOT_TOKEN) { bot = new Telegraf(BOT_TOKEN) }
+const env = Env.parse(process.env)
+
+const INTERVAL = 5_000 // Polling interval (in seconds)
+
+let bot: Telegraf | null
+if (env.BOT_TOKEN) { bot = new Telegraf(env.BOT_TOKEN) }
 
 const infiniteLoop = async () => {
-  console.log('running a circle')
+  const currentIP = await fetch('https://checkip.amazonaws.com/').then(res => res.text()).then(s => s.trim())
+  if (currentIP === env.IP) {
+    process.stdout.write('✅ ')
+  } else {
+    if (bot) {
+      bot.telegram.sendMessage('452184420', `🚨\n\nXKeen IP check failure.\nExpected IP: '${env.IP}'.\nReal IP: '${currentIP}'`)
+
+      // Temporary shit
+      await new Promise(resolve => setTimeout(resolve, 10 * 60 * 1_000));
+    }
+  }
 
   await new Promise(resolve => setTimeout(resolve, INTERVAL));
 
